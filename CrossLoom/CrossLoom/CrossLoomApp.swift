3f9d26142a9c44c604ae2f548916fe462a1cc29e
akito0011 <101452506+akito0011@users.noreply.github.com>
2025-06-23 12:23:08 +0200

    First Commit

---
+++ CrossLoom/CrossLoom/CrossLoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossLoomApp.swift
+//  CrossLoom
+//
+//  Created by Mohamed Tager on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CrossLoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrossLoom/CrossLoomTests/CrossLoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrossLoomTests.swift
+//  CrossLoomTests
+//
+//  Created by Mohamed Tager on 23/06/25.
+//
+
+import Testing
+@testable import CrossLoom
+
+struct CrossLoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
