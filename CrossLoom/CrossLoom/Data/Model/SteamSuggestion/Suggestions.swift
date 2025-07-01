49b56c6af3995fa93a6baa75876ebb6f18818aba
akito0011 <101452506+akito0011@users.noreply.github.com>
2025-07-01 12:25:53 +0200

    Updated and ficed Suggestion json decoding and List popoulation

--- CrossLoom/CrossLoom/Data/Model/SteamSuggestion/Suggestions.swift
+++ CrossLoom/CrossLoom/Data/Model/SteamSuggestion/Suggestions.swift
@@ -17,36 +17,63 @@ struct SuggestedGame: Identifiable {
         self.genres = []
 
         // Recupera i dettagli del gioco da Steam
-        await fetchGameDetails(gameID: id)
+        let details = await fetchGameDetails(gameID: id)
+        
+        if let details = details {
+            self.name = details.name
+            self.thumbnail = details.header_image
+            self.genres = details.genres.map({$0.description})
+        }
+        
+//        print(self)
     }
 
     // Funzione asincrona per fare la richiesta API
-    private mutating func fetchGameDetails(gameID: Int) async {
+    private mutating func fetchGameDetails(gameID: Int) async -> SteamGameResponseDetails? {
         // Costruisci l'URL dell'API di Steam
         let urlString = "https://store.steampowered.com/api/appdetails?appids=\(gameID)"
-        guard let url = URL(string: urlString) else { return }
-
+        guard let url = URL(string: urlString) else { return nil}
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-
             // Decodifica i dati ricevuti da Steam
             let decoder = JSONDecoder()
-            if let gameDetails = try? decoder.decode(SteamGameDetails.self, from: data) {
-                self.name = gameDetails.name
-                self.thumbnail = gameDetails.thumbnail
-                self.genres = gameDetails.genres
+
+            if let gameDetails = try? decoder.decode([String: SteamGameResponse].self, from: data) {
+                print("------------------------------")
+                print(gameDetails[String(id)]?.data.name)
+                return gameDetails[String(id)]?.data
             }
+            
         } catch {
-            print("Errore nel recuperare i dettagli del gioco: \(error)")
         }
+        return nil
     }
+        
     
 }
 
 // Struttura per i dettagli del gioco che riceviamo dall'API Steam
-struct SteamGameDetails: Decodable {
-    let id: Int
+
+struct SteamGameResponse: Codable {
+    let success: Bool
+    let data: SteamGameResponseDetails
+}
+
+struct SteamGameResponseDetails: Codable {
+//    let id: Int
+    let name: String
+    let header_image: String
+    let genres: [Genre]
+}
+
+struct SteamGameDetails: Codable {
+//    let id: Int
     let name: String
     let thumbnail: String
-    let genres: [String]
+    let genres: [Genre]
+}
+
+struct Genre: Codable {
+    let id: String
+    let description: String
 }
